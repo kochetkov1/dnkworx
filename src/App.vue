688d9ae8f27c3deb088d30d60f1e6bc362e37e4e
Dmitry Kochetkov <d.kochetkov1@yandex.ru>
2023-03-30 22:50:06 +0400

    first up

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <MainPage msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import MainPage from './components/MainPage.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MainPage
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+body {
+  margin: 0;
+  padding: 0;
+}
+</style>
---
+++ src/components/MainPage.vue
@@ -0,0 +1,56 @@
+<template>
+  <div ref='vantaRef' class="background">
+  </div>
+</template>
+
+<script>
+import * as THREE from "three";
+import HALO from 'vanta/dist/vanta.halo.min'
+// Make sure window.THREE is defined, e.g. by including three.min.js in the document head using a <script> tag
+
+export default {
+  mounted() {
+    this.vantaEffect = HALO({
+      el: this.$refs.vantaRef,
+      THREE: THREE
+    })
+  },
+  beforeUnmount() {
+    if (this.vantaEffect) {
+      this.vantaEffect.destroy()
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+.background {
+  min-width: 800px;
+  min-height: 800px;
+  height: 100%;
+}
+
+h3 {
+  margin: 40px 0 0;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
